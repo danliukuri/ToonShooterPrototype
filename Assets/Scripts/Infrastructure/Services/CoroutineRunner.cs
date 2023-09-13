0bc163adc84c05da5efd18328992f43fadc9f101
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-13 19:44:33 +0300

    Create coroutine runner

---
+++ Assets/Scripts/Infrastructure/Services/CoroutineRunner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ToonShooterPrototype.Infrastructure.Services
+{
+    public class CoroutineRunner : MonoBehaviour, ICoroutineRunner
+    {
+        public Coroutine Run(IEnumerator routine) => StartCoroutine(routine);
+
+        public void Stop(Coroutine coroutine) => StopCoroutine(coroutine);
+    }
+}
