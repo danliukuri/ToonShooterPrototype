2803af46248b80545de1eb6e24ac6c66e0266141
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-13 21:23:49 +0300

    Instantiate player on setup gameplay state enter

--- Assets/Scripts/Architecture/GameStates/Gameplay/SetupGameplayState.cs
+++ Assets/Scripts/Architecture/GameStates/Gameplay/SetupGameplayState.cs
@@ -1,9 +1,15 @@
+using ToonShooterPrototype.Features.Player;
 using ToonShooterPrototype.Utilities.Patterns.State;
+using UnityEngine.Pool;
 
 namespace ToonShooterPrototype.Architecture.GameStates.Gameplay
 {
     public class SetupGameplayState : IEnterableState
     {
-        public void Enter() => UnityEngine.Debug.Log(nameof(SetupGameplayState) + nameof(Enter));
+        private readonly IObjectPool<PlayerDataProvider> _playerPool;
+
+        public SetupGameplayState(IObjectPool<PlayerDataProvider> playerPool) => _playerPool = playerPool;
+
+        public void Enter() => _playerPool.Get();
     }
 }
---
+++ Assets/Scripts/Infrastructure/DependencyInjection/BindingsInstallers/SceneContext/PlayerBindingsInstaller.cs
@@ -0,0 +1,71 @@
+﻿using ToonShooterPrototype.Data.Dynamic;
+using ToonShooterPrototype.Data.Static.Configuration.Creation;
+using ToonShooterPrototype.Features.Player;
+using ToonShooterPrototype.Infrastructure.Creation.Factories;
+using UnityEngine;
+using UnityEngine.Pool;
+using Zenject;
+
+namespace ToonShooterPrototype.Infrastructure.DependencyInjection.BindingsInstallers.SceneContext
+{
+    public class PlayerBindingsInstaller : MonoInstaller
+    {
+        [SerializeField] private Transform spawnPoint;
+
+        [SerializeField] private FactoryConfig factoryConfig;
+        [SerializeField] private PoolConfig poolConfig;
+
+        public override void InstallBindings()
+        {
+            BindConfigurator();
+            BindFactory();
+            BindObjectPool();
+            BindData();
+        }
+
+        private void BindConfigurator()
+        {
+            Container
+                .BindInterfacesTo<PlayerConfigurator>()
+                .AsSingle()
+                .WithArguments(spawnPoint)
+                .WhenInjectedInto<DependentComponentFactory<PlayerDataProvider>>();
+        }
+
+        private void BindFactory()
+        {
+            Container
+                .BindInterfacesTo<FactoryConfig>()
+                .FromScriptableObject(factoryConfig)
+                .AsCached()
+                .WhenInjectedInto<DependentComponentFactory<PlayerDataProvider>>();
+
+            Container.BindInterfacesTo<DependentComponentFactory<PlayerDataProvider>>().AsSingle();
+        }
+
+        private void BindObjectPool()
+        {
+            Container
+                .BindInterfacesTo<PoolConfig>()
+                .FromScriptableObject(poolConfig)
+                .AsCached()
+                .WhenInjectedInto<ComponentPoolFactory<PlayerDataProvider>>();
+
+            Container
+                .Bind<IObjectPool<PlayerDataProvider>>()
+                .To<ObjectPool<PlayerDataProvider>>()
+                .FromFactory<ComponentPoolFactory<PlayerDataProvider>>()
+                .AsSingle();
+        }
+
+        private void BindData()
+        {
+            Container
+                .BindInterfacesAndSelfTo<PlayerData>()
+                .AsSingle()
+                .WhenInjectedInto<PlayerDataProvider>();
+        }
+    }
+}
+
+    
