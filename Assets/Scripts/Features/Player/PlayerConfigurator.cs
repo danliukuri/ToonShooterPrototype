f8106a83dc54d571e32e02b2e0c09ad049fb02b1
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-13 21:23:01 +0300

    Create player configurator

---
+++ Assets/Scripts/Features/Player/PlayerConfigurator.cs
@@ -0,0 +1,14 @@
+﻿using ToonShooterPrototype.Infrastructure.Creation.Services;
+using UnityEngine;
+
+namespace ToonShooterPrototype.Features.Player
+{
+    internal class PlayerConfigurator : IComponentConfigurator<PlayerDataProvider>
+    {
+        private readonly Transform _spawnPoint;
+
+        public PlayerConfigurator(Transform spawnPoint) => _spawnPoint = spawnPoint;
+
+        public void Configure(PlayerDataProvider component) => component.transform.position = _spawnPoint.position;
+    }
+}
