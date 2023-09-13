d15d9fef46278e552c441b63d43a7440eff0b4f9
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-13 19:52:11 +0300

    Bind coroutine runner dependency

---
+++ Assets/Scripts/Infrastructure/DependencyInjection/BindingsInstallers/ProjectContext/CoroutineRunnerBindingsInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Unity.VisualScripting;
+using Zenject;
+
+namespace ToonShooterPrototype.Infrastructure.DependencyInjection.BindingsInstallers.ProjectContext
+{
+    public class CoroutineRunnerBindingsInstaller : MonoInstaller
+    {
+        public override void InstallBindings() => BindCoroutineRunner();
+
+        private void BindCoroutineRunner() =>
+            Container.BindInterfacesTo<CoroutineRunner>().FromNewComponentOnNewGameObject().AsSingle();
+    }
+}
