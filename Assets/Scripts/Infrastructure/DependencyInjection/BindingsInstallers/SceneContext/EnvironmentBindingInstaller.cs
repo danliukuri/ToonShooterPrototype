20c56d09c9be893189bfb0acab2ca0f9b2a3af4b
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-16 14:10:53 +0300

    Bind environment dependencies

---
+++ Assets/Scripts/Infrastructure/DependencyInjection/BindingsInstallers/SceneContext/EnvironmentBindingInstaller.cs
@@ -0,0 +1,36 @@
+﻿using ToonShooterPrototype.Architecture.GameStates.Gameplay;
+using ToonShooterPrototype.Data.Static.Configuration;
+using ToonShooterPrototype.Features.Environment;
+using UnityEngine;
+using Zenject;
+
+namespace ToonShooterPrototype.Infrastructure.DependencyInjection.BindingsInstallers.SceneContext
+{
+    public class EnvironmentBindingInstaller : MonoInstaller
+    {
+        [SerializeField] private EnvironmentConfig environmentConfig;
+
+        public override void InstallBindings()
+        {
+            BindEnvironmentConfig();
+            BindGravityForceApplier();
+        }
+
+        private void BindEnvironmentConfig()
+        {
+            Container
+                .BindInterfacesAndSelfTo<EnvironmentConfig>()
+                .FromScriptableObject(environmentConfig)
+                .AsSingle()
+                .WhenInjectedInto<GravityForceApplier>();
+        }
+
+        private void BindGravityForceApplier()
+        {
+            Container
+                .BindInterfacesTo<GravityForceApplier>()
+                .AsSingle()
+                .WhenInjectedInto(typeof(TickableManager), typeof(SetupGameplayState));
+        }
+    }
+}
