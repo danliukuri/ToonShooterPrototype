9287e54171ed754ef38e8d7295a47bc3cb2d1294
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-16 14:08:50 +0300

    Make player groundable

--- Assets/Scripts/Data/Dynamic/PlayerData.cs
+++ Assets/Scripts/Data/Dynamic/PlayerData.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 
 namespace ToonShooterPrototype.Data.Dynamic
 {
-    public class PlayerData
+    public class PlayerData : IGroundable
     {
         public Transform Transform { get; set; }
         public CharacterController CharacterController { get; set; }
-        
+
         public PlayerConfig Config { get; set; }
+
+        public bool IsGrounded { get; set; }
+
+        private Vector3 _jumpingForce;
+        public ref Vector3 JumpingForce => ref _jumpingForce;
+
+        private Vector3 _movementForce;
+        public ref Vector3 MovementForce => ref _movementForce;
     }
 }
