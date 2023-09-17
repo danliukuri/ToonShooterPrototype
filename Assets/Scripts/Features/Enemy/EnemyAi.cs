02f5b6a46f4e812b30056481d9be19a87c5ee3c0
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-17 19:08:32 +0300

    Create enemy ai

---
+++ Assets/Scripts/Features/Enemy/EnemyAi.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ToonShooterPrototype.Data.Dynamic;
+using UnityEngine;
+using Zenject;
+
+namespace ToonShooterPrototype.Features.Enemy
+{
+    public class EnemyAi : ITickable
+    {
+        private readonly IEnumerable<EnemyData> _enemies;
+        private readonly PlayerData _player;
+
+        public EnemyAi(PlayerData player, IEnumerable<EnemyData> enemies)
+        {
+            _player = player;
+            _enemies = enemies;
+        }
+
+        public void Tick()
+        {
+            foreach (EnemyData enemy in _enemies)
+            {
+                bool isPlayerInSightRange = Vector3.Distance(enemy.Transform.position, _player.Transform.position) <=
+                                            enemy.Config.SightRange;
+
+                if (isPlayerInSightRange)
+                    ChasePlayer(enemy);
+            }
+        }
+
+        private void ChasePlayer(EnemyData enemy) => enemy.Agent.SetDestination(_player.Transform.position);
+    }
+}
