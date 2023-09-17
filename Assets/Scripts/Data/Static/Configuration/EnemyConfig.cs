850394b06aaa01ae6619c44e6bf8d74310da2f1a
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-17 19:06:59 +0300

    Create enemy configuration

---
+++ Assets/Scripts/Data/Static/Configuration/EnemyConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ToonShooterPrototype.Data.Static.Configuration
+{
+    [CreateAssetMenu(fileName = nameof(EnemyConfig), menuName = "Configuration/Enemy")]
+    public class EnemyConfig : ScriptableObject
+    {
+        [field: SerializeField, Min(default)] public float SightRange { get; private set; }
+    }
+}
