77a918f523fd113f9aa2a79ef0db2a96578a1d4e
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-17 19:07:32 +0300

    Create player data

---
+++ Assets/Scripts/Data/Dynamic/EnemyData.cs
@@ -0,0 +1,16 @@
+﻿using ToonShooterPrototype.Data.Static.Configuration;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ToonShooterPrototype.Data.Dynamic
+{
+    public class EnemyData
+    {
+        public EnemyData(EnemyConfig config) => Config = config;
+
+        public EnemyConfig Config { get; }
+        
+        public Transform Transform { get; set; }
+        public NavMeshAgent Agent { get; set; }
+    }
+}
---
+++ Assets/Scripts/Features/Enemy/EnemyDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using ToonShooterPrototype.Data.Dynamic;
+using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+
+namespace ToonShooterPrototype.Features.Enemy
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyDataProvider : MonoBehaviour
+    {
+        public EnemyData Data { get; private set; }
+        
+        [Inject]
+        public void Construct(EnemyData data) => Data = data;
+
+        private void Awake()
+        {
+            Data.Agent = GetComponent<NavMeshAgent>();
+            Data.Transform = GetComponent<Transform>();
+        }
+    }
+}
