0bfcbdc6d0a3b87267d904675cc9cca8f72f4f10
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-18 14:10:50 +0300

    Make enemy shoot at player if he's in shoot range

--- Assets/Scripts/Features/Enemy/EnemyAi.cs
+++ Assets/Scripts/Features/Enemy/EnemyAi.cs
@@ -9,9 +9,11 @@ namespace ToonShooterPrototype.Features.Enemy
     {
         private readonly IEnumerable<EnemyData> _enemies;
         private readonly PlayerData _player;
+        private readonly IEnemyShooter _shooter;
 
-        public EnemyAi(PlayerData player, IEnumerable<EnemyData> enemies)
+        public EnemyAi(PlayerData player, IEnumerable<EnemyData> enemies, IEnemyShooter shooter)
         {
+            _shooter = shooter;
             _player = player;
             _enemies = enemies;
         }
@@ -20,11 +22,14 @@ namespace ToonShooterPrototype.Features.Enemy
         {
             foreach (EnemyData enemy in _enemies)
             {
-                bool isPlayerInSightRange = Vector3.Distance(enemy.Transform.position, _player.Transform.position) <=
-                                            enemy.Config.SightRange;
+                float distanceToPlayer = Vector3.Distance(enemy.Transform.position, _player.Transform.position);
+                bool isPlayerInSightRange = distanceToPlayer <= enemy.Config.SightRange;
+                bool isPlayerInShootRange = distanceToPlayer <= enemy.Config.ShootRange;
 
                 if (isPlayerInSightRange)
                     ChasePlayer(enemy);
+                if (isPlayerInShootRange && _shooter.IsAbleToShoot)
+                    _shooter.Shoot(enemy, _player.Transform.position + enemy.Config.ShootHeight * Vector3.up);
             }
         }
 
