045f18845231217b1a5726f5eeea91810d4c0e55
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-18 19:34:49 +0300

    Set shooter data for player

--- Assets/Scripts/Data/Static/Configuration/PlayerConfig.cs
+++ Assets/Scripts/Data/Static/Configuration/PlayerConfig.cs
@@ -1,12 +1,18 @@
-﻿using UnityEngine;
+﻿using ToonShooterPrototype.Data.Dynamic;
+using UnityEngine;
 
 namespace ToonShooterPrototype.Data.Static.Configuration
 {
     [CreateAssetMenu(fileName = nameof(PlayerConfig), menuName = "Configuration/Player/Data")]
-    public class PlayerConfig : ScriptableObject
+    public class PlayerConfig : ScriptableObject, IShooterData
     {
         [field: SerializeField, Min(default)] public float MoveSpeed { get; private set; }
         [field: SerializeField, Min(default)] public float SprintSpeed { get; private set; }
         [field: SerializeField, Min(default)] public float JumpPower { get; private set; }
+
+        [field: SerializeField, Min(default)] public float ShotAccuracy { get; private set; }
+        [field: SerializeField, Min(default)] public float ShootRange { get; private set; }
+        [field: SerializeField, Min(default)] public float ShootDelay { get; private set; }
+        [field: SerializeField, Min(default)] public float ShootHeight { get; private set; }
     }
 }
