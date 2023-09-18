729cef5f112e61ea2228d59b8e85eb1fd2ad0188
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-18 19:30:42 +0300

    Create raycast bullet VFX spawner

---
+++ Assets/Scripts/Features/Bullets/RaycastBulletVfxSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Pool;
+
+namespace ToonShooterPrototype.Features.Bullets
+{
+    public class RaycastBulletVfxSpawner : IRaycastBulletVfxSpawner
+    {
+        private readonly IObjectPool<RaycastBulletsVfxMarker> _bulletsVfxPool;
+
+        public RaycastBulletVfxSpawner(IObjectPool<RaycastBulletsVfxMarker> bulletsVfxPool) =>
+            _bulletsVfxPool = bulletsVfxPool;
+
+        public void Spawn(RaycastHit hit)
+        {
+            RaycastBulletsVfxMarker raycastBulletsVfx = _bulletsVfxPool.Get();
+            raycastBulletsVfx.transform.position = hit.point;
+            raycastBulletsVfx.transform.rotation = Quaternion.LookRotation(hit.normal);
+        }
+    }
+}
