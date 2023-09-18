3130ffbb1b964983782f34d9aa630a6aa1d45d66
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-18 19:38:22 +0300

    Create shoot input service

---
+++ Assets/Scripts/Infrastructure/Services/Input/ShootInputService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Zenject;
+
+namespace ToonShooterPrototype.Infrastructure.Services.Input
+{
+    public class ShootInputService : ITickable, IShootInputService
+    {
+        private const string ShootButtonName = "Fire1";
+        
+        public event Action ShootButtonPressed;
+        
+        public void Tick()
+        {
+            if (UnityEngine.Input.GetButtonDown(ShootButtonName))
+                ShootButtonPressed?.Invoke();
+        }
+    }
+}
