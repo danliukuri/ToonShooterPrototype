815a3593175bf46343fbaaf7e8d8391ffac5ab66
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-18 19:26:41 +0300

    Extract shooter data from enemy configuration

---
+++ Assets/Scripts/Data/Dynamic/IShooterData.cs
@@ -0,0 +1,10 @@
+﻿namespace ToonShooterPrototype.Data.Dynamic
+{
+    public interface IShooterData
+    {
+        float ShotAccuracy { get; }
+        float ShootRange { get; }
+        float ShootDelay { get; }
+        float ShootHeight { get; }
+    }
+}
