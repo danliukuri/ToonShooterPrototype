284f527f459a778dc3d972fcb8210d3971c6633f
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-19 12:39:27 +0300

    Configure shooting weapons damage

--- Assets/Scripts/Data/Static/Configuration/ShootingWeaponConfig.cs
+++ Assets/Scripts/Data/Static/Configuration/ShootingWeaponConfig.cs
@@ -8,5 +8,6 @@ namespace ToonShooterPrototype.Data.Static.Configuration
         [field: SerializeField, Min(default)] public float ShotAccuracy { get; private set; }
         [field: SerializeField, Min(default)] public float ShootRange { get; private set; }
         [field: SerializeField, Min(default)] public float ShootDelay { get; private set; }
+        [field: SerializeField, Min(default)] public int ShootDamage { get; private set; }
     }
 }
