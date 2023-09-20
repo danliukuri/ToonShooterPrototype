c5fef9dc55aa92bee521147e55c7ce75d9b3fef4
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-09-20 22:04:52 +0300

    Create player progress data saver

---
+++ Assets/Scripts/Data/Dynamic/PlayerProgressData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace ToonShooterPrototype.Data.Dynamic
+{
+    [Serializable]
+    public class PlayerProgressData
+    {
+        [field: SerializeField] public int WinCount { get; set; }
+        [field: SerializeField] public int LoseCount { get; set; }
+    }
+}
---
+++ Assets/Scripts/Features/SaveSystem/IPlayerProgressSaver.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ToonShooterPrototype.Data.Dynamic;
+
+namespace ToonShooterPrototype.Features.SaveSystem
+{
+    public interface IPlayerProgressSaver
+    {
+        Task Save(PlayerProgressData playerProgressData);
+    }
+}
